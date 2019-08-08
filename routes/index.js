300dd363a82becbc13b3e4e81bb60049b25eb380
beatricecourage <bea_courage@hotmail.co.uk>
2019-08-08 15:51:53 +0100

    Functioning nodeJS app with json objects

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,25 @@
+// import React from "react";
+// ReactDOM.render(element, document.getElementById('root'));
+//
+
 var express = require('express');
 var router = express.Router();
+var dataa = require('../data.json');
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'my first demo app',
-  name: 'bea'});
+  name: 'bea',
+    sd: dataa
+  });
 });
-
 
 
 //So when user requests home page--> perform function and send a response, so render the index page and create some javascript variables.
 //Between the curly braces is where you define js variables.
 
-
-
 module.exports = router;
-
 
 
 //user requests index page in app.js, go to indexrouter, where it says render the index page
---
+++ sparecpde.js
@@ -0,0 +1,16 @@
+<p><% sd.color.forEach(function(item){%></p>
+<li><% item.color%></li>
+<% });%>
+
+<ul>
+<% for(var i=0; i<5; i++){%>
+<li>hi</li>
+    <%}%>
+</ul>
+
+
+
+
+
+
+
